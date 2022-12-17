0b50b83aeab2d947e019fd60ba9cded47b991ee7
dr-roshyara <mathematikboy@yahoo.com>
2022-12-17 10:19:03 +0100

    guard problem

--- backend1/src/router/register/guard.ts
+++ backend1/src/router/register/guard.ts
@@ -3,6 +3,7 @@ import { CacheEnum } from '@/enum/CacheEnum'
 import store from '@/utils/store'
 import type { RouteLocationNormalized, Router } from 'vue-router'
 import errorStore from '@/stores/errorStore'
+import { logout } from '@/utils/helper'
 
 class Guard {
   constructor(private router: Router) {}
@@ -15,6 +16,10 @@ class Guard {
     errorStore().resetError()
     const _token = this.token()
     const _requiresAuth = to.meta.requiresAuth
+    if (to.path == '/login') {
+      store.remove(CacheEnum.TOKEN_NAME)
+      localStorage.clear() // always clean localStorage before reset the state
+    }
     // console.log(_requiresAuth)
     // console.log(_token)
     // console.log('from: ')
